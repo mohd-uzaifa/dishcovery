1a46acd1e99dfcb5c1fbd42971559de8c59c6496
mohd-uzaifa <mohduzaifa02@gmail.com>
2026-07-10 20:26:38 +0530

    update the DTO

--- backend/src/main/java/com/project/dishcovery/RecipeSummaryDTO.java
+++ backend/src/main/java/com/project/dishcovery/RecipeSummaryDTO.java
@@ -1,14 +1,18 @@
 package com.project.dishcovery;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class RecipeSummaryDTO {
     private Long id;
     private String name;
     private String image;
+    @JsonProperty("prep_time")
     private String prepTime;
     private String diet;
     private String course;
+    private String cuisine;
     private List<String> ingredients;
 
     public RecipeSummaryDTO(Recipe recipe) {
@@ -18,6 +22,7 @@ public class RecipeSummaryDTO {
         this.prepTime = recipe.getPrepTime();
         this.diet = recipe.getDiet();
         this.course = recipe.getCourse();
+        this.cuisine = recipe.getCuisine();
         this.ingredients = recipe.getIngredients();
     }
     public Long getId() {
@@ -43,8 +48,12 @@ public class RecipeSummaryDTO {
     public String getCourse() {
         return course;
     }
+    public String getCuisine() {
+        return cuisine;
+    }
 
     public List<String> getIngredients() {
         return ingredients;
     }
+
 }
